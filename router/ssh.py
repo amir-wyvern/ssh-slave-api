e08dd232a11ca779938378e0f8256cdb05789228
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-04-05 17:17:08 +0330

    fix create router

--- router/ssh.py
+++ router/ssh.py
@@ -21,14 +21,24 @@ def user_info(request: SshAccount):
     
     try:
         home_dir = "/home/" + request.username
+        username = request.username
+        password = request.password
 
-        subprocess.run(["useradd", "-m", "-d", home_dir, request.username])
-        subprocess.run(["passwd", request.username], input= request.password.encode())
+        subprocess.run(["useradd", "-m", "-d", home_dir, username])
 
+        passwd_cmd = ['passwd', username]
+        p = subprocess.Popen(passwd_cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = p.communicate(input=f"{password}\n{password}".encode())
+
+        if p.returncode != 0:
+            raise HTTPException(status_code=500 ,detail={'message': f"Failed to set password for user {username}: {stderr.decode()}", 'internal_code': 1500})
+        
+        else:
+            return f"Password set successfully for user {username}"
+        
     except Exception as e:
         raise HTTPException(status_code=500, detail={'message':f'Exception : {e}', 'internal_code': 1500})
     
-    return True
 
 @router.delete('/delete' ,responses= {500: {'model': HTTPError}})
 def update_user_profile(request: DeleteSsh):
@@ -42,10 +52,9 @@ def update_user_profile(request: DeleteSsh):
             raise HTTPException(status_code=500 ,detail={'message': f'Failed to delete user {request.username}: {stderr.decode()}', 'internal_code': 1500})
         
         else:
-            print(f"User {request.username} deleted successfully")
+            return f"User {request.username} deleted successfully"
 
     except Exception as e:
-
         raise HTTPException(status_code=500 ,detail={'message': f'Exception : {e}', 'internal_code': 1500})
 
 @router.post('/block')
@@ -60,3 +69,8 @@ def update_user_password(request: BlockSsh):
 
     subprocess.run(['gpasswd', '-d', request.username, 'blockUsers'])
     return True
+
+
+
+#Match User *,!wyvern,!root
+#       ForceCommand /home/res.sh
