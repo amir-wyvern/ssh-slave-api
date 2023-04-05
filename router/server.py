7e4818939183efbcefe2d2ee75433146f3761192
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-04-05 18:11:21 +0330

    style: clean up

--- router/server.py
+++ router/server.py
@@ -19,14 +19,16 @@ from typing import List
 router = APIRouter(prefix='/server', tags=['server'])
 
 @router.post('/init')
-def user_info(request: InitServer, token: str= Depends(get_auth)):
+def set_config_server(request: InitServer, token: str= Depends(get_auth)):
     
     if os.path.isfile('/home/init.txt'):
         raise HTTPException(status_code=404, detail={'internal_code': 1404, 'message': 'init config already done!'})
     
     with open('/home/restricted.sh', 'w') as f:
         f.write('#!/bin/rbash\necho "Connected!"\nwhile true; do\nread -p " " cmd\ndone')
+    
     subprocess.run(["chmod", "o+rx", '/home/restricted.sh'])
+
     try:
         
         subprocess.run(['groupadd', 'blockUsers'])
@@ -62,17 +64,21 @@ def user_info(request: InitServer, token: str= Depends(get_auth)):
         passwd_cmd = ['passwd', username]
         p = subprocess.Popen(passwd_cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = p.communicate(input=f"{password}\n{password}".encode())
-
-        if p.returncode != 0:
-            raise HTTPException(status_code=500 ,detail={'message': f"Failed to set password for user {username}: {stderr.decode()}", 'internal_code': 1500})
-        
-        subprocess.run(['usermod', '-aG', 'sudo', 'manager'])
-        subprocess.run(['systemctl', 'restart', 'sshd'])
-    
-        return 'Server Successfuly initial'
     
     except Exception as e:
         raise HTTPException(status_code=500, detail={'message': str(Exception), 'internal_code': 1500})
+
+    if p.returncode != 0:
+        raise HTTPException(status_code=500 ,detail={'message': f"Failed to set password for user {username}: {stderr.decode()}", 'internal_code': 1500})
+    
+    try:
+        subprocess.run(['usermod', '-aG', 'sudo', 'manager'])
+        subprocess.run(['systemctl', 'restart', 'sshd'])
+        
+    except Exception as e:
+        raise HTTPException(status_code= 500, detail={'message': f'error {e}', 'internal_code': 1500})
+    
+    return 'Server Successfuly initial'
     
 
 @router.get('/users', response_model= List[str])
